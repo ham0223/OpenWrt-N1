7382776e1fdbf828fb73817914d9d0eb6b171d2c
ham0223 <126130711+ham0223@users.noreply.github.com>
2025-03-02 21:31:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,4 +39,4 @@ sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
 rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
